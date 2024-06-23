e9395c92abff6a15382d90ba3e4162fe387a56dc
Ricky Junaidi <106050611+RickyJ2@users.noreply.github.com>
2024-06-23 18:59:17 +0700

    remove unused define

--- AGV_arduino/main/Header.h
+++ AGV_arduino/main/Header.h
@@ -3,13 +3,3 @@
 #include "MotorDriver.h"
 #include "Proximity.h"
 #include "voltageReader.h"
-
-#define IDLE 0 
-#define AUTO 1
-#define MANUAL 2
-
-#define STOP 0
-#define FORWARD 1
-#define LEFT 2
-#define RIGHT 3
-#define BACKWARD 4
