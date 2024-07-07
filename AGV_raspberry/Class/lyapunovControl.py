aa3edba31d60f2c4724dfb60a3095f47a3898021
Ricky Junaidi <106050611+RickyJ2@users.noreply.github.com>
2024-07-07 12:05:04 +0700

    Modified Lyapunov

--- AGV_raspberry/Class/lyapunovControl.py
+++ AGV_raspberry/Class/lyapunovControl.py
@@ -2,10 +2,9 @@ import math
 from Class.pose import Pose
 
 class LyapunovControl:
-    def __init__(self, k1, k2, k3, tolerance):
+    def __init__(self, k1, k2, tolerance):
         self.k1 = k1
         self.k2 = k2
-        self.k3 = k3
         self.tolerance = tolerance
     
     def compute(self, currentPoint: Pose, targetPoint: Pose) -> tuple[float, float]:
@@ -13,5 +12,13 @@ class LyapunovControl:
         if abs(error.point.x) < self.tolerance and abs(error.point.y) < self.tolerance and abs(error.orientation) < math.radians(180):
             return 0, 0
         v = self.k1 * (error.point.x * math.cos(currentPoint.orientation) + error.point.y * math.sin(currentPoint.orientation))
-        omega = self.k2 * (-error.point.x * math.sin(currentPoint.orientation) + error.point.y * math.cos(currentPoint.orientation)) + self.k3 * (error.orientation)
+        omega = -self.k2 * error.orientation
+
+        if error.orientation > math.pi:
+            error.orientation -= 2 * math.pi
+        if error.orientation < -math.pi:
+            error.orientation += 2 * math.pi
+        if error.orientation > math.pi/2 or error.orientation < -math.pi/2:
+            v = 0
+        
         return v, omega
